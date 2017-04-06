a235633bc9ec971a78966f66749fe17586764e48
Hidalgo Pomales <hidalgo.pomales@upr.edu>
2017-04-05 20:37:03 -0400

    Added intersection, union, complement, median.

--- Probabilius.py
+++ Probabilius.py
@@ -52,3 +52,110 @@ def calculateMode(list):
 
 
 
+def calculateMedian(list):
+    sortedList= sorted(list)
+    listLength =len(list)
+    midIndex =(listLength-1)/2
+
+    # If the list is empty, then there is no median
+    if( listLength< 1):
+        return None
+
+    # If the length of the list is odd, then return the number in the middle
+    if(listLength % 2==1):
+        return sortedList[midIndex]
+
+    # If the length of the list even, then return the average of the numbers in the middle
+    else:
+        return((sortedList[midIndex]+sortedList[midIndex+1])/2.0)
+
+
+def calculateUnion(list1,list2):
+    tempList = []
+
+    # Find which one is the bigger list
+    if (len(list1) < len(list2)):
+        short = list1
+        bigger = list2
+    else:
+        short = list2
+        bigger = list1
+
+    # Iterate through the small one
+    for element in short:
+        tempList.append(element)
+
+    # Iterate through the bigger one and add items that doesnt exist in the list
+    for element in bigger:
+        if element not in tempList:
+            tempList.append(element)
+
+    return tempList
+
+
+def calculateIntersection(list1,list2):
+
+    tempList =[]
+
+    # If they have the same length, there we can iterate using just one loop
+    if (len(list1) == len(list2)):
+        for element in list1 and list2:
+            if element in list1 and list2:
+                tempList.append(element)
+
+    # If not, then we need to find the bigger one to avoid exceptions
+    else:
+         if len(list1) > len(list2):
+             bigger= list1
+             smaller = list2
+         else:
+            bigger=list2
+            smaller = list1
+
+    # Since the intersections needs to be in both of the list we just go through the smaller one
+    # because everything after the size of the smaller one, can not be an intersection.
+         for element in smaller:
+             if element in bigger:
+                 if element not in tempList:
+                    tempList.append(element)
+    return tempList
+
+
+def calculateComplement(list1, list2):
+    tempList = []
+
+    # If they have the same length, there we can iterate using just one loop
+    if (len(list1) == len(list2)):
+        for element,element2 in zip(list1,list2):
+                if element not in list2 and element2 not in list1:
+                    if element not in tempList:
+                        tempList.append(element)
+                    if element2 not in tempList:
+                        tempList.append(element2)
+
+
+    # If not, then we need to find the bigger one to avoid exceptions
+    else:
+        if len(list1) > len(list2):
+            bigger = list1
+            smaller = list2
+        else:
+            bigger = list2
+            smaller = list1
+
+        # Since the complements needs to be in one list and not in the other, we go through the smaller one
+        # first to avoid exceptions.
+        for element in smaller:
+            if element not in bigger:
+                if element not in tempList:
+                    tempList.append(element)
+
+        # Add the rest of the items that are not in the smaller one.
+        for element2 in bigger:
+                if element2 not in smaller:
+                    if element2 not in tempList:
+                        tempList.append(element2)
+    return tempList
+
+
+
